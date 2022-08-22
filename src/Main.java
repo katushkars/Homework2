27c5519a2b7071c0907ff47d64c95c40adf70393
Екатерина <katushkars@mail.ru>
2022-08-22 23:24:36 +0500

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание 1
+        var cat = 7;
+        byte box =1;
+        short box2 = 200;
+        int box3 = 2147483646;
+        long box4 = 999999999999999L;
+        float box5 =1.9f;
+        double box6 = 56.88;
+        System.out.println(box5);
     }
 }
