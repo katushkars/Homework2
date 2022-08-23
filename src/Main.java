5deed2b32039a13901d68dfed607cebb685b9cc3
Екатерина <katushkars@mail.ru>
2022-08-23 14:59:43 +0500

    задание 2 - выполнено

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,15 @@ public class Main {
         float box5 =1.9f;
         double box6 = 56.88;
         System.out.println(box6);
+
+        //Задание 2
+        double boxer1=78.2;
+        double boxer2=82.7;
+        double allBoxerWeight = boxer1+boxer2;
+        double weightDefference = boxer1-boxer2;
+        System.out.println("Общий вес боксеров " + allBoxerWeight);
+        System.out.println("Разница в весе первого и второго боксера "+ weightDefference);
+
+
     }
 }
