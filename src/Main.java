d09b0f7d12fc6153f6b95ef5ec86954563e28345
Екатерина <katushkars@mail.ru>
2022-08-23 15:31:24 +0500

    задание 3 - выполнено

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,25 @@ public class Main {
         System.out.println("Общий вес боксеров " + allBoxerWeight);
         System.out.println("Разница в весе первого и второго боксера "+ weightDefference);
 
+        //Задание 3
+        byte banan = 5;
+        int bananWeight = banan *80;
+        int milk = 200;
+        double milkWeight = milk*1.05;
+        byte iceCream = 2;
+        int iceCreamWeight = iceCream*100;
+        byte egg = 4;
+        int eggWeight = egg*70;
+        double breakfastWeight = bananWeight+milkWeight+iceCreamWeight+eggWeight;
+        double breakfastWeightKg= breakfastWeight/1000;
+        System.out.println("Общий вес завтрака в граммах "+breakfastWeight);
+        System.out.println("Общий вес завтрака в кг "+breakfastWeightKg);
+
+
+
+
+
+
 
     }
 }
